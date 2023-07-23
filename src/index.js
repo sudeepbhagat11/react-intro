9e7815a6598cd1dd16afe0919fe94532f2178062
sudeepbhagat11 <130081182+sudeepbhagat11@users.noreply.github.com>
2023-07-23 08:50:50 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>JSX practice</h1>
+    <ul>
+      <li> React</li>
+      <li> Node js</li>
+      <l1> Javascript</l1>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
